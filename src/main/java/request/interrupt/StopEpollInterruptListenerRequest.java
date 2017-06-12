a25b83f54f7991acddaee4db16b084c1d61c3704
Miloslav Žežulka <zezulka95@gmail.com>
2017-06-12 12:58:00 +0000

    import fix

--- src/main/java/request/interrupt/StopEpollInterruptListenerRequest.java
+++ src/main/java/request/interrupt/StopEpollInterruptListenerRequest.java
@@ -2,7 +2,11 @@ package request.interrupt;
 
 public final class StopEpollInterruptListenerRequest extends AbstractEpollInterruptListenerRequest {
 
+<<<<<<< HEAD
     private static final String PREFIX = "INTR_STOPPED";
+=======
+    private static final String PREFIX = "INTR_STOP";
+>>>>>>> 56aaf63c4dfb58906460112874bda94f4b30cddd
     private static final InterruptListenerManager MANAGER = EpollInterruptListenerManager.getInstance();
 
     public StopEpollInterruptListenerRequest(InterruptListenerArgs arg) {
@@ -16,6 +20,10 @@ public final class StopEpollInterruptListenerRequest extends AbstractEpollInterr
 
     @Override
     public void handleRequest() {
+<<<<<<< HEAD
         MANAGER.deregisterInput(super.getArg());
+=======
+        MANAGER.deregisterInputs(super.getArg());
+>>>>>>> 56aaf63c4dfb58906460112874bda94f4b30cddd
     }
 }
